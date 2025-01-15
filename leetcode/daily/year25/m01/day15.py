689719e3cf7ec631414d1355412cfa2968da3c28
chunhang.xch <1205241831@qq.com>
2025-01-15 13:44:49 +0800

    feat: implement solution for LeetCode problem 3066 - Minimum Operations to Exceed Threshold II

---
+++ leetcode/daily/year25/m01/day15.py
@@ -0,0 +1,13 @@
+from heapq import heapify, heappop, heappush
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        heapify(nums)
+        ans = 0
+        while len(nums) > 1 and nums[0] < k:
+            x, y = heappop(nums), heappop(nums)
+            heappush(nums, min(x, y) * 2 + max(x, y))
+            ans += 1
+        return ans
